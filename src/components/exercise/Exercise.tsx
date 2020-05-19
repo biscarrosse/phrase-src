3a33dd75e4bbdf40768f42295f629e55c5f83a90
Ales Klasek <klasek.ales@gmail.com>
2020-05-19 20:01:28 +0200

    Cleanup Exercise

--- src/components/exercise/Exercise.tsx
+++ src/components/exercise/Exercise.tsx
@@ -1,41 +1,19 @@
 // React:
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 // Redux:
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AppState } from '../../store/store';
-import { showAnswer, showQuestion } from '../../store/exercise/actions';
-// Style:
-import {
-  Horizontal,
-  Vertical,
-  Margin,
-  SpaceBetween
-} from '../styles/common.style';
-// import { Btn } from '../styles/button.style';
-import { H1, H2, P } from '../styles/text.style';
-// import * as PALETTE from '../styles/colors.style';
-// Components:
-import Card from '../card/Card';
-import Button from '../button/Button';
+
 import ExerciseAnswer from './ExerciseAnswer';
 import ExerciseQuestion from './ExerciseQuestion';
-// Constants:
-import * as SIZE from '../../constants/buttonSizes';
-import * as TEXT from '../../constants/commonText';
-import * as COLOR from '../../constants/buttonColors';
 
 const Exercise = () => {
-  const [question, setQuestion] = useState(false);
-
   const { isQuestion } = useSelector((state: AppState) => state.exercise);
 
   useEffect(() => {
     console.log('isQuestion toggle', isQuestion);
   }, [isQuestion]);
 
-  const one = () => console.log('one');
-  const two = () => console.log('two');
-  const three = () => console.log('three');
   // show card --> slide to see answer via react-spring
 
   const result = isQuestion ? <ExerciseQuestion /> : <ExerciseAnswer />;
--- src/store/exercise/types.tsx
+++ src/store/exercise/types.tsx
@@ -16,81 +16,171 @@ export interface ExerciseAction {
   payload?: BlockOf100 | Phrases;
 }
 
+// tier 5:
 export interface Phrase {
   id: string;
   english: string;
   spanish: string;
   czech: string;
 }
+// tier 4:
 export interface Phrases {
   phrases: Phrase[];
 }
+// tier 3:
 export interface BlockOf10 {
   [key: string]: Phrases;
 }
+// tier 2:
+export interface BlockOfLevels {
+  [key: string]: BlockOf10;
+}
+// tier 1:
 export interface BlockOf100 {
   name: string;
-  data_of_100: BlockOf10;
+  data_of_100: BlockOfLevels;
 }
+
 // {
-//   "name": "block_of_100_phrases",
+//   "name": "language_data",
 //   "data_of_100": {
-//     "block_10": {
-//       "phrases": [
-//         {
-//           "id": 3,
-//           "english": "english 3",
-//           "spanish": "spanish 3",
-//           "czech": "czech 3"
-//         },
-//         {
-//           "id": 4,
-//           "english": "english 4",
-//           "spanish": "spanish 4",
-//           "czech": "czech 4"
-//         }
-//       ]
+//     "level_A": {
+//       "block_0": {
+//         "phrases": [
+//           {
+//             "id": 1,
+//             "english": "english 1",
+//             "spanish": "spanish 1",
+//             "czech": "czech 1"
+//           },
+//           {
+//             "id": 2,
+//             "english": "english 2",
+//             "spanish": "spanish 2",
+//             "czech": "czech 2"
+//           },
+//           {
+//             "id": 3,
+//             "english": "english 3",
+//             "spanish": "spanish 3",
+//             "czech": "czech 3"
+//           }
+//         ]
+//       },
+//       "block_1": {
+//         "phrases": [
+//           {
+//             "id": 11,
+//             "english": "english 11",
+//             "spanish": "spanish 11",
+//             "czech": "czech 11"
+//           },
+//           {
+//             "id": 12,
+//             "english": "english 12",
+//             "spanish": "spanish 12",
+//             "czech": "czech 12"
+//           },
+//           {
+//             "id": 13,
+//             "english": "english 13",
+//             "spanish": "spanish 13",
+//             "czech": "czech 13"
+//           }
+//         ]
+//       }
 //     },
-//     "block_20": {
-//       "phrases": [
-//         {
-//           "id": 3,
-//           "english": "english 3",
-//           "spanish": "spanish 3",
-//           "czech": "czech 3"
-//         },
-//         {
-//           "id": 4,
-//           "english": "english 4",
-//           "spanish": "spanish 4",
-//           "czech": "czech 4"
-//         }
-//       ]
+//     "level_B": {
+//       "block_0": {
+//         "phrases": [
+//           {
+//             "id": 1,
+//             "english": "english 1",
+//             "spanish": "spanish 1",
+//             "czech": "czech 1"
+//           },
+//           {
+//             "id": 2,
+//             "english": "english 2",
+//             "spanish": "spanish 2",
+//             "czech": "czech 2"
+//           },
+//           {
+//             "id": 3,
+//             "english": "english 3",
+//             "spanish": "spanish 3",
+//             "czech": "czech 3"
+//           }
+//         ]
+//       },
+//       "block_1": {
+//         "phrases": [
+//           {
+//             "id": 11,
+//             "english": "english 11",
+//             "spanish": "spanish 11",
+//             "czech": "czech 11"
+//           },
+//           {
+//             "id": 12,
+//             "english": "english 12",
+//             "spanish": "spanish 12",
+//             "czech": "czech 12"
+//           },
+//           {
+//             "id": 13,
+//             "english": "english 13",
+//             "spanish": "spanish 13",
+//             "czech": "czech 13"
+//           }
+//         ]
+//       }
+//     },
+//     "level_C": {
+//       "block_0": {
+//         "phrases": [
+//           {
+//             "id": 1,
+//             "english": "english 1",
+//             "spanish": "spanish 1",
+//             "czech": "czech 1"
+//           },
+//           {
+//             "id": 2,
+//             "english": "english 2",
+//             "spanish": "spanish 2",
+//             "czech": "czech 2"
+//           },
+//           {
+//             "id": 3,
+//             "english": "english 3",
+//             "spanish": "spanish 3",
+//             "czech": "czech 3"
+//           }
+//         ]
+//       },
+//       "block_1": {
+//         "phrases": [
+//           {
+//             "id": 11,
+//             "english": "english 11",
+//             "spanish": "spanish 11",
+//             "czech": "czech 11"
+//           },
+//           {
+//             "id": 12,
+//             "english": "english 12",
+//             "spanish": "spanish 12",
+//             "czech": "czech 12"
+//           },
+//           {
+//             "id": 13,
+//             "english": "english 13",
+//             "spanish": "spanish 13",
+//             "czech": "czech 13"
+//           }
+//         ]
+//       }
 //     }
 //   }
-// }
-
-// {
-//   name: "test",
-//   items: {
-//       "a": {
-//           id: 1,
-//           size: 10
-//       },
-//       "b": {
-//           id: 2,
-//           size: 34
-//       }
-//   }
-// }
-// export interface Item {
-//   id: number;
-//   size: number;
-// }
-
-// export interface Example {
-//   name: string;
-//   items: {
-//     [key: string]: Item;
-//   };
 // }
